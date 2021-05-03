356a2be07e6404cd50c2a3102b596d0940893304
Viktor M <mitinvn@gmail.com>
2021-05-03 10:18:26 +0300

    nice

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var _ = requre('lodash');
 
 var css = document.querySelector("h3");
@@ -20,3 +21,4 @@ function setBackground() {
 
 	css.textContent = body.style.background + ";";	
 }
+},{}]},{},[1]);
